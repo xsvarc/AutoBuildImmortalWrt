b93c050e975147cdf3a5f96289db73dfc27ca20e
xsvarc <xsvarc@gmail.com>
2025-03-24 16:12:04 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -132,7 +132,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="2025.3.21"
+NEW_DESCRIPTION="2025.3.24"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
